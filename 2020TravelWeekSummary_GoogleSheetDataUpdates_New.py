874e32a93140fc8ac19c39d52896cc039f576768
rrlinrr <45771006+rrlinrr@users.noreply.github.com>
2022-03-23 12:17:59 -0400

    Update 2020TravelWeekSummary_GoogleSheetDataUpdates_New.py

--- 2020TravelWeekSummary_GoogleSheetDataUpdates_New.py
+++ 2020TravelWeekSummary_GoogleSheetDataUpdates_New.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import pygsheets
 import os
+import json
+from google.oauth2 import service_account
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
