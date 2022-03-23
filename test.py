93d3d42523d5476ab2159381399b5921d3d5fb0b
rrlinrr <45771006+rrlinrr@users.noreply.github.com>
2022-03-23 15:26:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,3 +11,12 @@ pd.set_option('display.max_rows', None)
 
 gapi = os.environ.get('GAPI')
 print(gapi)
+
+gapiStr = json.dumps(gapi)
+
+SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
+service_account_info = json.loads(gapiStr)
+print("service_account_info")
+print(type(service_account_info))
+print(service_account_info)
+my_credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
