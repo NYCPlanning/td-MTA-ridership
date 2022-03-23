f6516a0aa99071a77081cd529f7d21f172ea9be6
rrlinrr <45771006+rrlinrr@users.noreply.github.com>
2022-03-23 15:10:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,11 @@ import pandas as pd
 import numpy as np
 import pygsheets
 import os
+import json
+from google.oauth2 import service_account
 
-print("Hello! 120 Broadway!")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+gapi = os.environ.get('GAPI')
+print(gapi)
